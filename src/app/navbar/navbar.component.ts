49ff79cea735cfb61f2c9733577eb8b1e19a27c5
Daniel Sogl <me@danielsogl.de>
2024-05-22 10:41:55 +0200

    add paper-design

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {
+  sidebarVisible: boolean = false;
+
+  sidebarToggle() {
+    var body = document.getElementsByTagName('body')[0];
+
+    if (this.sidebarVisible == false) {
+      body.classList.add('nav-open');
+      this.sidebarVisible = true;
+    } else {
+      this.sidebarVisible = false;
+      body.classList.remove('nav-open');
+    }
+  }
+}
