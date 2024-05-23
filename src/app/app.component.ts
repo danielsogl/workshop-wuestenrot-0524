864a32b6518f7bcd5c8e1c2b520e77af7f5b1f6a
Daniel Sogl <me@danielsogl.de>
2024-05-23 14:36:48 +0200

    add next flight module for router demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,17 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FlightSearchComponent } from './flight-search/flight-search.component';
 import { ConfigService } from './shared/config.service';
+import { NextFlightsModule } from './next-flights/next-flights.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [SidebarComponent, NavbarComponent, FlightSearchComponent],
+  imports: [
+    SidebarComponent,
+    NavbarComponent,
+    FlightSearchComponent,
+    NextFlightsModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
-import { ApplicationConfig, DEFAULT_CURRENCY_CODE } from '@angular/core';
+import { ApplicationConfig, DEFAULT_CURRENCY_CODE, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
+import { NextFlightsModule } from './next-flights/next-flights.module';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(),
+    importProvidersFrom(NextFlightsModule),
     { provide: DATE_PIPE_DEFAULT_OPTIONS, useValue: { format: 'dd.MM.yyyy' } },
     { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
   ],
